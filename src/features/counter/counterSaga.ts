b1c4ec1fbd7301e63d06ad5c41394ee8b0e4f4a8
quangnguyen90 <nguyenphuquang90@gmail.com>
2021-06-06 19:04:04 +0700

    2.2- Redux-saga effect example: takeEvery

--- src/features/counter/counterSaga.ts
+++ src/features/counter/counterSaga.ts
@@ -1,3 +1,14 @@
+import { takeEvery } from "@redux-saga/core/effects";
+import { PayloadAction } from "@reduxjs/toolkit";
+import { increment } from "./counterSlice";
+
+export function* log(action: PayloadAction){
+    console.log('log', action);
+}
+
 export default function* counterSaga() {
     console.log('counter saga');
+    // yield takeEvery('counter/increment', log);
+    // yield takeEvery(increment().type, log);
+    yield takeEvery('*', log);
 }
