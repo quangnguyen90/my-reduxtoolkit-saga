4b659b886fed18e6da34bd1348f7b384926c7df2
quangnguyen90 <nguyenphuquang90@gmail.com>
2021-06-13 03:16:50 +0700

    3.1 Redux-saga effect example: takeEvery (P2)

--- src/features/counter/counterSaga.ts
+++ src/features/counter/counterSaga.ts
@@ -1,14 +1,26 @@
 import { takeEvery } from "@redux-saga/core/effects";
 import { PayloadAction } from "@reduxjs/toolkit";
-import { increment } from "./counterSlice";
+import { delay, put } from "redux-saga/effects";
+import { incrementSaga, incrementSagaSuccess } from "./counterSlice";
 
-export function* log(action: PayloadAction){
-    console.log('log', action);
+// export function* log(action: PayloadAction){
+//     console.log('log', action);
+// }
+
+function* handleIncrementSaga(action: PayloadAction<number>) {
+    console.log('Waiting 1s');
+    // wait 1s
+    yield delay(1000);
+    console.log('Waiting done, dispatch action');
+
+    // dispatch action success
+    yield put(incrementSagaSuccess(action.payload));
 }
 
 export default function* counterSaga() {
     console.log('counter saga');
     // yield takeEvery('counter/increment', log);
     // yield takeEvery(increment().type, log);
-    yield takeEvery('*', log);
+    // yield takeEvery('*', log);
+    yield takeEvery(incrementSaga.toString(), handleIncrementSaga);
 }
